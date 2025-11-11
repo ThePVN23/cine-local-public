c863e175a0efb924fba69f7b3b370c908721e4b1
pateldev1804 <pateldev1804@gmail.com>
2025-11-10 20:26:27 -0500

    better file strucutre

--- cine-local/src/app/App.tsx
+++ cine-local/src/app/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import LandingPage from "./pages/LandingPage";
-import BrowsePage from "./pages/BrowsePage";
+
 import MovieDetailPage from "./pages/MovieDetailPage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
@@ -11,7 +11,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/browse" element={<BrowsePage />} />
+     
         <Route path="/movie/:id" element={<MovieDetailPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
